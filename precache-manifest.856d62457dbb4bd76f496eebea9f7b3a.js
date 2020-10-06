90055b4da07339177c0c88eed1900aa5ef8970d2
Aoda-dev <66174034+Aoda-dev@users.noreply.github.com>
2020-10-06 20:04:27 +0600

    Add files via upload

---
+++ precache-manifest.856d62457dbb4bd76f496eebea9f7b3a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2480d4f92e19b396343f07ddf229fdbd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "768734ef24a966e15c90",
+    "url": "./static/css/main.b4b87752.chunk.css"
+  },
+  {
+    "revision": "b277fbe89a5597862534",
+    "url": "./static/js/2.e8ef17ae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.e8ef17ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "768734ef24a966e15c90",
+    "url": "./static/js/main.0fcf2890.chunk.js"
+  },
+  {
+    "revision": "430bcccc9b65f324de2e",
+    "url": "./static/js/runtime-main.dbb54a1e.js"
+  },
+  {
+    "revision": "ddf79d109a14330abf0e114916cddb19",
+    "url": "./static/media/demonSlayer1.ddf79d10.mp3"
+  },
+  {
+    "revision": "677c3bc0da053acd7552b99079ff71ce",
+    "url": "./static/media/katawaredoki.677c3bc0.mp3"
+  },
+  {
+    "revision": "d668c23b8296f079d73c63310d27f5d2",
+    "url": "./static/media/spotify-logo.d668c23b.png"
+  }
+]);
